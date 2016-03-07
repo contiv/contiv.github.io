dfd1e020871dc5c882badc00ee34ea7983af3965
Sukhesh Halemane <sukhesh_h@yahoo.com>
2016-03-06 23:59:35 -0800

    Add blog section

--- websrc/config.rb
+++ websrc/config.rb
@@ -5,3 +5,10 @@ activate :hashicorp do |h|
   h.version     = "0.2.0"
   h.github_slug = "contiv/netplugin"
 end
+
+activate :blog do |blog|
+  # set options on blog
+  blog.prefix = "articles"
+  blog.default_extension = ".md"
+  blog.layout = "article"
+end
